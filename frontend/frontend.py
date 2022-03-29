b7cc5b4d7fa6d62986864eec7776831738c69fb0
Alex C-G <alexcg@outlook.com>
2022-03-29 11:57:58 +0200

    docs(frontend): links, explanations

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -24,6 +24,10 @@ def get_docs():
 img_da, txt_da = get_docs()
 
 st.header("CLIP-as-service")
+
+st.sidebar.header("What is CLIP-as-service?")
+st.markdown("[**CLIP-as-service**](https://clip-as-service.jina.ai/) is a low-latency high-scalability service from [Jina AI](https://github.com/jina-ai/) for embedding images and text. It can be easily integrated as a microservice into [neural search](https://docs.jina.ai/get-started/neural-search/) solutions.")
+
 st.sidebar.header("Options")
 mode = st.sidebar.radio(label="Input type", options=["Text-to-image", "Image-to-text"])
 server = st.sidebar.text_input(
@@ -33,7 +37,7 @@ c = Client(server)
 
 if mode == "Text-to-image":
     st.markdown(
-        "### Text to image\n Match text input strings to images from the Totally-looks-like dataset"
+        "### Text to image\n Match text input strings to images from the [Totally-looks-like dataset](https://sites.google.com/view/totally-looks-like-dataset)"
     )
     input_text = st.text_input(label="Text input")
     txt_search_button = st.button(label="Search")
@@ -72,7 +76,7 @@ elif mode == "Image-to-text":
     st.markdown(
         "In this demo we'll take an input image and search for the closest matching lines from *Pride and Prejudice*"
     )
-    random_button = st.button(label="I feel lucky (random image)")
+    random_button = st.button(label="I feel lucky (random image from Totally Looks Like dataset)")
     if random_button:
         query_image = img_da.sample(1)
         st.markdown("### Input image")
