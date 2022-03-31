72e8ef1e93c0ac96a4b6fc0be553ee47acbd8cd8
Alex C-G <alexcg@outlook.com>
2022-03-31 16:18:50 +0200

    fix: params

--- frontend/utils/data_to_docarray_push.py
+++ frontend/utils/data_to_docarray_push.py
@@ -5,13 +5,18 @@ input_file = "./data/quotes.csv"
 content_field = "quote"
 max_docs = 10000000 # set to high number for all
 encoder = "CLIPEncoder"
+docarray_pushed_name = "text-quotes-500k-embeddings"
+
+max_docs = 10 # for testing
 
 # Create from csv
 docs = DocumentArray.from_csv(input_file, field_resolver={"quote": "text"}, size=max_docs)
+print(docs[0])
 
 # Encode
-executor = Executor.from_hub(f"jinahub://{encoder}")
-executor.encode(docs)
+executor = Executor.from_hub(f"jinahub://{encoder}", install_requirements=True)
+executor.encode(docs, parameters={})
+print(docs[0].embedding)
 
 # Push
-docs.push("text-quotes-500k-embeddings", show_progress=True)
+docs.push(docarray_pushed_name, show_progress=True)
