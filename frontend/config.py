c922b2d22da1e273600513022652e563459b6524
Alex C-G <alexcg@outlook.com>
2022-04-01 15:51:49 +0200

    fix: official cas demo url

--- frontend/config.py
+++ frontend/config.py
@@ -1,7 +1,8 @@
 import os
 sources = ["ttl-textual", "ttl-embedding"]  # text  # images
 
-clip_server = os.getenv("SERVER", "grpc://examples.jina.ai:51000")
+clip_server = os.getenv("SERVER", "grpc://demo-cas.jina.ai:51000")
 sqlite_filename = "clip-as-service.db"
 table_name = "clip_as_service"
-data_sources = ["ttl-embedding", "ttl-textual"]
+data_sources = ["ttl-embedding", "ttl-textual"] # ttl, pride and prejudice
+data_sources = ["ttl-embedding", "text-quotes-500-embeddings"] # ttl, quotes dataset
